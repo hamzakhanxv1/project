ed10ab76342bab7e0daeda24e83414079f84c454
hamzakhanxv1 <147655339+hamzakhanxv1@users.noreply.github.com>
2024-05-31 12:01:07 +0500

    first commit

---
+++ app/src/main/java/com/hamza/pizzeria/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.anas.pizzeria;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Colors
+        Window window = SplashScreen.this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(SplashScreen.this, R.color.colorPrimaryYellow));
+        window.setNavigationBarColor(ContextCompat.getColor(SplashScreen.this, R.color.colorPrimaryYellow));
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
+
